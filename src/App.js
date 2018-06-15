0c30e241cf31a9772c1f8cf1223dec9332891f79
Leopold Roos <leopold.roos@tv4.se>
2018-06-15 11:36:46 +0200

    Create-react App is set up.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src='/images/weblogo.png' className="App-logo" alt="logo" />
+          <h1 className="App-title">Image AI</h1>
+        </header>
+        <p className="App-intro">
+          Lets do some smart stuff!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
